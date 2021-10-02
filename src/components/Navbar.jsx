027e873e988bfefd89b83a2d033b937031a6ceb0
Evgenii Kolesnikov <jekow@yandex.ru>
2021-10-02 03:09:44 +0300

    Add mobile navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,23 @@ import {
   MenuOutlined,
 } from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png';
+import { useEffect, useState } from 'react';
 
 export const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    screenSize < 768 ? setActiveMenu(false) : setActiveMenu(true);
+  }, [screenSize]);
+
   return (
     <div className='nav-container'>
       <div className='logo-container'>
@@ -17,24 +32,29 @@ export const Navbar = () => {
         <Typography.Title level={2} className='logo'>
           <Link to='/'>Crypto</Link>
         </Typography.Title>
-        {/* <Button className='menu-control-container'>
-
-        </Button> */}
+        <Button 
+          className='menu-control-container'
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme='dark'>
-        <Menu.Item key='1' icon={<HomeOutlined />}>
-          <Link to='/'>Home</Link>
-        </Menu.Item>
-        <Menu.Item key='2' icon={<FundOutlined />}>
-          <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item key='3' icon={<MoneyCollectOutlined />}>
-          <Link to='/exchanges'>Exchanges</Link>
-        </Menu.Item>
-        <Menu.Item key='4' icon={<BulbOutlined />}>
-          <Link to='/news'>News</Link>
-        </Menu.Item>
-      </Menu>
+      {activeMenu && (
+        <Menu theme='dark' onClick={() => setActiveMenu(false)}>
+          <Menu.Item key='1' icon={<HomeOutlined />}>
+            <Link to='/'>Home</Link>
+          </Menu.Item>
+          <Menu.Item key='2' icon={<FundOutlined />}>
+            <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item key='3' icon={<MoneyCollectOutlined />}>
+            <Link to='/exchanges'>Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item key='4' icon={<BulbOutlined />}>
+            <Link to='/news'>News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   );
 };
